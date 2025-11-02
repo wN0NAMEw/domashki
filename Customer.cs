75126f7f8cf9fe5876e2778f821c7b114d19c563
wN0NAMEw <150360386+wN0NAMEw@users.noreply.github.com>
2025-11-02 18:34:19 +0700

    Delete Customer.cs

--- Customer.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace chetonovoe
-{
-    public class Customer
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
-    }
-}
